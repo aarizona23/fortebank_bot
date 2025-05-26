7f897ec0dee982aa36b0c3f8995c78768947337b
aarizona23 <2003ariana.sadyr@gmail.com>
2025-05-26 19:20:30 +0500

    added session

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fortebot.model import ServiceSearch
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+from starlette.middleware.sessions import SessionMiddleware
+import os
 
-chat_history = []
+secret_key = os.getenv("SESSION_KEY")
+app = FastAPI()
+
+app.add_middleware(SessionMiddleware, secret_key=secret_key)
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 def get_chat(request: Request):
@@ -14,7 +19,7 @@ def get_chat(request: Request):
     :param request:
     :return: HTMLResponse with the chat history
     """
-
+    chat_history = request.session.get("chat_history", [])
     return templates.TemplateResponse("index.html", {
         "request": request,
         "chat_history": chat_history
@@ -30,6 +35,7 @@ def post_chat(request: Request, user_message: str = Form(...)):
     :return: HTMLResponse with the updated chat history
     """
 
+    chat_history = request.session.get("chat_history", [])
     chat_history.append({"sender": "user", "message": user_message})
 
     searcher = ServiceSearch(user_message)
@@ -38,6 +44,8 @@ def post_chat(request: Request, user_message: str = Form(...)):
         chat_history.append({"sender": "bot", "message": bot_response["text"], "url": bot_response["url"]})
     else:
         chat_history.append({"sender": "bot", "message": bot_response["text"]})
+
+    request.session["chat_history"] = chat_history
 
     return templates.TemplateResponse("index.html", {
         "request": request,
